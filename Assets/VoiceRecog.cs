a8b122d6dca235c251b77e6a47a13073afb2c6b9
Eoghan O'Connor <oconnorjohn40@gmail.com>
2020-02-26 14:20:01 +0000

    basic Speech Detection

---
+++ Assets/VoiceRecog.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class VoiceRecog : MonoBehaviour
+{
+    public string[] keywords = new string[] { "a", "b", "c", "d" };
+    public ConfidenceLevel confidence = ConfidenceLevel.Low;
+    public float speed = 1;
+
+    public Text results;
+    public Image target;
+
+    protected PhraseRecognizer recognizer;
+    protected string word = "d";
+
+    private void Start()
+    {
+        if (keywords != null)
+        {
+            recognizer = new KeywordRecognizer(keywords, confidence);
+            recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
+            recognizer.Start();
+        }
+    }
+
+    private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        word = args.text;
+        results.text = "You said: <b>" + word + "</b>";
+        //Debug.Log(word);
+    }
+
+    private void Update()
+    {
+        var x = target.transform.position.x;
+        var y = target.transform.position.y;
+
+        switch (word)
+        {
+            case "a":
+                y += speed;
+                break;
+            case "b":
+                y -= speed;
+                break;
+            case "c":
+                x -= speed;
+                break;
+            case "d":
+                x += speed;
+                break;
+        }
+
+        target.transform.position = new Vector3(x, y, 0);
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (recognizer != null && recognizer.IsRunning)
+        {
+            recognizer.OnPhraseRecognized -= Recognizer_OnPhraseRecognized;
+            recognizer.Stop();
+        }
+    }
+}
